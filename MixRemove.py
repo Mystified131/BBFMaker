965bb5a4dc2f318b988cf5dd1a70c0d4b0d96c15
Thomas Park <mystifiedthomas@gmail.com>
2021-01-12 06:59:52 -0600

    Tweaking

---
+++ MixRemove.py
@@ -0,0 +1,36 @@
+pl = []
+
+infile = open("Zimmer.m3u", "r")
+
+plist = infile.readline()
+
+while plist:
+    pl.append(plist.strip())
+    plist = infile.readline()
+
+infile.close()
+
+playlist = []
+
+for elem in pl:
+    if "mix" not in elem and "Mix" not in elem:
+        playlist.append(elem)
+        
+oustr = "Zimmer.m3u"
+
+outfile = open("temp.txt", "w")
+
+for elem in spl:
+    outfile.write(elem + '\n')
+
+outfile.close()   
+
+outfile = open(oustr, "w")
+
+with open("temp.txt") as f: 
+    for line in f: 
+        if not line.isspace(): 
+            #sys.stdout.write(line)
+            outfile.write(line)
+
+outfile.close()
